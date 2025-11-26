08182334e748f505357e9bffdb10b847357a674e
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2025-11-26 18:31:01 +0600

    home banner and footer done

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,18 @@
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      
+    <div>
+      <section className="text-center py-20">
+        <h1 className="text-4xl font-bold">
+          Welcome to <span className="text-blue-600">Bangla Bazar</span>
+        </h1>
+        <p className="text-gray-600 mt-4 max-w-xl mx-auto">
+          Buy the best tech products at an affordable price.
+        </p>
+        <button className="mt-6 px-6 py-3 bg-blue-600 text-white rounded-lg">
+          Shop Now
+        </button>
+      </section>
     </div>
   );
 }
