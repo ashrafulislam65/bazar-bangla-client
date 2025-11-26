a62186a7fd8e06662e805e73352b4f0ee712f66f
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2025-11-26 18:25:57 +0600

    initial setup done

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer(props) {
+    return (
+        <footer className="bg-gray-900 text-white py-6 mt-20">
+            <div className="container mx-auto text-center">
+                © 2025 Bangla Bazar — All Rights Reserved.
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
